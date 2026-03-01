6c26b6c3272b95efb5537d253e1908b54a3c8b91
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-01 15:15:52 -0300

    Impled components BtnRedirect

---
+++ src/shared/components/btnRedirect/BtnRedirect.tsx
@@ -0,0 +1,22 @@
+import { useRouter } from 'next/navigation';
+import {ButtonBase} from "@/shared/components/btn/btn.component";
+import {BtnRedirectProps} from "@/shared/components/btnRedirect/BtnRedirect.interface";
+
+export const BtnRedirect = ({ icon, text, to, children, ...props }: BtnRedirectProps) => {
+    const router = useRouter();
+
+    const handleNavigation = () => {
+        router.push(to);
+    };
+
+    return (
+        <ButtonBase
+            onClick={handleNavigation}
+            {...props}
+        >
+            {children}
+            {text}
+            {icon}
+        </ButtonBase>
+    );
+};
