c28797a0b6b0c6e5edddf49180650415ad0f6672
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-02 15:50:19 -0300

    improvements in AppError

--- src/core/exceptions/AppError.ts
+++ src/core/exceptions/AppError.ts
@@ -1,9 +1,11 @@
 export class AppError extends Error {
     public readonly statusCode: number;
+    public readonly errors?: Record<string, string[]>;
 
-    constructor(message: string, statusCode = 400) {
+    constructor(message: string, statusCode = 400, errors?: Record<string, string[]>) {
         super(message);
         this.statusCode = statusCode;
+        this.errors = errors;
         this.name = 'AppError';
     }
 }
