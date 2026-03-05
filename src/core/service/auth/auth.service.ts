f481d78d6d7f71e53064ccf06e211544560cd885
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-05 15:38:14 -0300

    improvement in method setTokens and added: isLogged and login

--- src/core/service/auth/auth.service.ts
+++ src/core/service/auth/auth.service.ts
@@ -2,6 +2,7 @@ import {CreateUserDto} from "@/core/dto/user/create-user.dto";
 import {api} from "@/core/api/api-client";
 import {ResponseHTTP} from "@/core/res/response-http.res";
 import {TokenResponse} from "@/core/res/token-response.res";
+import {LoginUserDto} from "@/core/dto/user/login-user.dto";
 
 export class AuthService {
     private readonly path: string = 'v1/Auth'
@@ -10,11 +11,29 @@ export class AuthService {
         return await api.post<ResponseHTTP<unknown>>(this.path+'/register', dto);
     }
 
+    async login(dto: LoginUserDto) {
+        return await api.post<ResponseHTTP<unknown>>(this.path+'/login', dto);
+    }
+
+    isLogged(): boolean {
+        const token: string | null = localStorage.getItem('token');
+
+        return !!token;
+    }
+
     setTokens(token: TokenResponse) {
-        localStorage.setItem('token', token.token);
-        localStorage.setItem('tokenExp', token.expirationToken.toISOString());
-        localStorage.setItem('refreshToken', token.refreshToken);
-        localStorage.setItem('refreshTokenExp', token.expirationRefreshToken.toISOString());
+        try {
+            localStorage.setItem('token', token.token);
+            localStorage.setItem('refreshToken', token.refreshToken);
+            if (token.expirationRefreshToken) {
+                localStorage.setItem('refreshTokenExp', token.expirationRefreshToken.toString());
+            }
+            if (token.expirationToken) {
+                localStorage.setItem('tokenExp', token.expirationToken.toString());
+            }
+        } catch (e) {
+            console.error(e);
+        }
     }
 
     logout() {
