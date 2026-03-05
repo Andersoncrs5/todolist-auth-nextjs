15a79975dee8d218a14b44be44c90399606d9bb2
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-05 16:38:11 -0300

    Improvements in Register

--- src/app/auth/register/page.tsx
+++ src/app/auth/register/page.tsx
@@ -1,82 +1,87 @@
 'use client'
 
 import useRegister from "@/app/auth/register/register.hook";
-
-import {BtnSubmit} from "@/shared/components/btnSubmit/btnSubmit.component";
+import { BtnSubmit } from "@/shared/components/btnSubmit/btn-submit.component";
 import CustomInput from "@/shared/components/input/input.component";
 import PasswordInput from "@/shared/components/passwordInput/password-input.component";
 
 export default function Register() {
-
     const {
-        handleSubmitForm,
-        name,
-        setName,
-        email,
-        setEmail,
-        password,
-        setPassword,
+        handleSubmit,
+        onSubmit,
+        register,
         isSubmiting,
         errors
-    } = useRegister()
+    } = useRegister();
 
     return (
         <div className="grid place-items-center min-h-screen">
+            <div className="w-full max-w-md p-8 rounded-2xl border">
+                <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className="flex flex-col gap-6"
+                >
 
-            <div className="w-full max-w-md p-8 rounded-2xl border ">
+                    <div>
+                        <CustomInput
+                            label="Name"
+                            {...register("name", {
+                                required: "Name is required",
+                                minLength: 2,
+                                maxLength: 100
+                            })}
+                            className="rounded-lg px-4 py-2 bg-white/10 border border-white/30 text-white"
+                        />
+                        {errors.name && (
+                            <span className="text-red-500 text-sm">
+                                {errors.name.message}
+                            </span>
+                        )}
+                    </div>
 
-                <form onSubmit={handleSubmitForm} className="flex flex-col gap-6">
+                    <div>
+                        <CustomInput
+                            label="Email"
+                            type="email"
+                            {...register("email", {
+                                required: "Email is required",
+                                minLength: 8,
+                                maxLength: 150
+                            })}
+                            className="rounded-lg px-4 py-2 bg-white/10 border border-white/30 text-white"
+                        />
+                        {errors.email && (
+                            <span className="text-red-500 text-sm">
+                                {errors.email.message}
+                            </span>
+                        )}
+                    </div>
 
-                    <CustomInput
-                        value={name}
-                        label="Name"
-                        type="text"
-                        onChange={(e) => setName(e.target.value)}
-                        className={"rounded-lg px-4 py-2 bg-white/10 border border-white/30 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-blue-500  transition duration-200"}
-                    />
-
-                    {errors.Name && (
-                        <span className="text-red-500 text-sm">
-                            {errors.Name.message as string}
-                        </span>
-                    )}
-
-                    <CustomInput
-                        value={email}
-                        label="Email"
-                        type="email"
-                        onChange={(e) => setEmail(e.target.value)}
-                        className={"rounded-lg px-4 py-2 bg-white/10 border border-white/30 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-blue-500  transition duration-200"}
-                    />
-
-                    {errors.Email && (
-                        <span className="text-red-500 text-sm">
-                            {errors.Email.message as string}
-                        </span>
-                    )}
-
-                    <PasswordInput
-                        value={password}
-                        label="Password"
-                        onChange={(e) => setPassword(e.target.value)}
-                        className={"rounded-lg px-4 py-2 bg-white/10 border border-white/30 text-white placeholder-white/60 focus:outline-none focus:ring-2 focus:ring-blue-500  transition duration-200"}
-                    />
-
-                    {errors.Password && (
-                        <span className="text-red-500 text-sm">
-                            {errors.Password.message as string}
-                        </span>
-                    )}
+                    <div>
+                        <PasswordInput
+                            label="Password"
+                            {...register("password", {
+                                required: "Password is required",
+                                minLength: 8,
+                                maxLength: 50,
+                            })}
+                            className="rounded-lg px-4 py-2 bg-white/10 border border-white/30 text-white"
+                        />
+                        {errors.password && (
+                            <span className="text-red-500 text-sm">
+                                {errors.password.message}
+                            </span>
+                        )}
+                    </div>
 
                     <BtnSubmit
-                        className="mt-2 py-3 rounded-lg bg-blue-600 hover:bg-blue-700 transition duration-300"
+                        className="mt-2 py-3 rounded-lg bg-blue-600 hover:bg-blue-700"
                         text="Create Account"
                         isSubmiting={isSubmiting}
                     />
 
                 </form>
             </div>
-
         </div>
-    )
+    );
 }
