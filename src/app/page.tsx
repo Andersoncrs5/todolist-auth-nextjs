88b93f867e0ce11b48b234b752fa2d82b788c6a3
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-05 21:53:32 -0300

    Added logic checkLogged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,28 @@
 'use client'
 
-import { BtnRedirect } from "@/shared/components/btnRedirect/BtnRedirect";
+import { BtnRedirect } from "@/shared/components/btnRedirect/btn-redirect";
+import { useRouter } from "next/navigation";
+import { AuthService } from "@/core/service/auth/auth.service";
+import { useEffect, useMemo } from "react";
 
 export default function Home() {
+    const router = useRouter();
+    const authService = useMemo(() => new AuthService(), []);
+
+    useEffect(() => {
+        const checkLogin = () => {
+            const result = authService.isLogged();
+            if (result) {
+                router.push("/main");
+            }
+        };
+
+        checkLogin();
+    }, [authService, router]);
+
     return (
         <div className="grid place-items-center min-h-screen">
-
             <div className="grid grid-cols-3 gap-6 text-white">
-
                 <div className="col-span-3 text-center">
                     <h1 className="text-5xl md:text-7xl font-extrabold tracking-[0.3em] font-serif text-white drop-shadow-lg">
                         WELCOME
@@ -26,9 +41,7 @@ export default function Home() {
                         className="bg-blue-900 border px-6 py-2 rounded-lg hover:bg-transparent transition duration-300"
                     />
                 </div>
-
             </div>
-
         </div>
     );
 }
