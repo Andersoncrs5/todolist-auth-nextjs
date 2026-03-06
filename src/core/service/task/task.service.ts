9b203463e1c8cec6b95806adc9b68f2c7c6a9a7d
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-05 21:52:59 -0300

    New method getAll

--- src/core/service/task/task.service.ts
+++ src/core/service/task/task.service.ts
@@ -1,3 +1,13 @@
+import {api} from "@/core/api/api-client";
+import {TaskQueryParams} from "@/core/dto/task/task-query.params";
+
 export class TaskService {
     private readonly path: string = 'v1/Task'
+
+    async getAll(query: TaskQueryParams) {
+        return await api.get(this.path, {
+            params: query
+        });
+    }
+
 }
