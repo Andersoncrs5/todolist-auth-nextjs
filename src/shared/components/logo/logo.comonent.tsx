f406c4b5fb99d930ecf40910e5f8554b6206677c
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-07 16:55:28 -0300

    New component Logo

---
+++ src/shared/components/logo/logo.comonent.tsx
@@ -0,0 +1,7 @@
+export default function Logo() {
+    return (
+        <h2 className="text-white font-bold text-xl tracking-wider">
+            TODO<span className="text-blue-500">LIST</span>
+        </h2>
+    )
+}
