3929eb2c46f481a62885b49af84624810addc92a
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-08 19:44:22 -0300

    Change type column priority

--- src/core/dto/task/create-task.dto.ts
+++ src/core/dto/task/create-task.dto.ts
@@ -1,9 +1,11 @@
+import {TaskPriority} from "@/core/enums/task-priority .enum";
+
 export class CreateTaskDto {
     title: string;
     description: string;
-    priority: number;
+    priority: TaskPriority;
 
-    constructor(title: string, description: string, priority: number) {
+    constructor(title: string, description: string, priority: TaskPriority) {
         this.title = title;
         this.description = description;
         this.priority = priority;
