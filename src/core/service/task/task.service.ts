4ce3307feaa2afb4fdb64516a8e891e445bc2610
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-08 19:43:02 -0300

    new method create

--- src/core/service/task/task.service.ts
+++ src/core/service/task/task.service.ts
@@ -1,5 +1,6 @@
 import {api} from "@/core/api/api-client";
 import {TaskQueryParams} from "@/core/dto/task/task-query.params";
+import {CreateTaskDto} from "@/core/dto/task/create-task.dto";
 
 export class TaskService {
     private readonly path: string = 'v1/Task'
@@ -12,6 +13,10 @@ export class TaskService {
 
     async delete(id: number) {
         return await api.delete(this.path+"/"+id)
+    }
+
+    async create(dto: CreateTaskDto) {
+        return await api.post(this.path, dto);
     }
 
 }
