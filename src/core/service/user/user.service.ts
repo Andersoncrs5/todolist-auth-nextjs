280f00dac732966eb0773bbbf5f09fc78cf7a5d9
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-11 14:20:32 -0300

    New method getUser and delete

--- src/core/service/user/user.service.ts
+++ src/core/service/user/user.service.ts
@@ -1,3 +1,13 @@
+import {api} from "@/core/api/api-client";
+
 export class UserService {
     private readonly path: string = 'v1/User'
+
+    async getUser() {
+        return api.get(`${this.path}/me`)
+    }
+
+    async delete() {
+        return api.delete(`${this.path}`)
+    }
 }
