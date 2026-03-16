7dc580cc47cf9fa661377a90b6fa701799d4eb41
andersoncrs5 <anderson.c.rms2005@gmail.com>
2026-03-16 15:45:49 -0300

    New method update

--- src/core/service/user/user.service.ts
+++ src/core/service/user/user.service.ts
@@ -1,4 +1,5 @@
 import {api} from "@/core/api/api-client";
+import {UpdateUserDto} from "@/core/dto/user/update-user.dto";
 
 export class UserService {
     private readonly path: string = 'v1/User'
@@ -9,5 +10,10 @@ export class UserService {
 
     async delete() {
         return api.delete(`${this.path}`)
+    }
+
+    async update(data: UpdateUserDto) {
+        data.name = data.name.trim()
+        return api.put(`${this.path}`, data)
     }
 }
